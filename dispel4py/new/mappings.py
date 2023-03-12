71daf8ee49bdcf86788e3f895dcc76c9d1e26c41
Ben Vardy <bv20@st-andrews.ac.uk>
2023-03-12 12:58:23 +0000

    Remove unused mappings

--- dispel4py/new/mappings.py
+++ dispel4py/new/mappings.py
@@ -1,10 +1,5 @@
 config = {
-    "mpi": "dispel4py.new.mpi_process",
     "multi": "dispel4py.new.multi_process",
     "simple": "dispel4py.new.simple_process",
-    "storm": "dispel4py.storm.storm_submission",
-    "spark": "dispel4py.new.spark_process",
-    "zmq_multi": "dispel4py.new.zmq_multi",
-    "dyn_multi": "dispel4py.new.dynamic",
     "redis": "dispel4py.new.dynamic_redis"
 }
